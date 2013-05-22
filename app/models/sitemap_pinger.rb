1a9c3b4192b84e71a84888307d11ed15a587f0ab
Rubén Salinas <rubensalinasgarcia@gmail.com>
2013-05-22 12:25:47 +0200

    Removed ask

--- app/models/sitemap_pinger.rb
+++ app/models/sitemap_pinger.rb
@@ -1,7 +1,6 @@
 class SitemapPinger 
   SEARCH_ENGINES = {
     google: "http://www.google.com/webmasters/tools/ping?sitemap=%s",
-    ask: "http://submissions.ask.com/ping?sitemap=%s",
     bing: "http://www.bing.com/webmaster/ping.aspx?siteMap=%s"
   }
 
@@ -10,11 +9,12 @@ class SitemapPinger
     SEARCH_ENGINES.each do |name, url|
       request = url % CGI.escape("http://www.agilesoft.es/sitemap.xml")  
       SitemapLogger.info "  Pinging #{name} with #{request}"
-      if Rails.env == "production"
-        response = Net::HTTP.get_response(URI.parse(request))
+
+      #if Rails.env == "production"
+        response = Net::HTTP.get_response(URI(request))
         SitemapLogger.info "    #{response.code}: #{response.message}"
         SitemapLogger.info "    Body: #{response.body}"
-      end
+      #end
     end
   end
 end
